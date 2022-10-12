1f7218c93e9af164acf2bb5291e81ad719407782
Felipe Alves Louzeiro <felipe.louzeiro@external.bfmarketing.net>
2022-10-12 11:42:40 -0300

    ✨ history_view - feat: implementando timeline

--- history_view/src/App.tsx
+++ history_view/src/App.tsx
@@ -1,5 +1,52 @@
+import { Timeline } from './components/timeline/timeline'
+import rings from './assets/rings.svg'
+import office from './assets/office.svg'
+
 function App() {
-  return <div>history</div>
+  return (
+    <div className="container-fluid">
+      <div className="container">
+        <Timeline history={data} />
+      </div>
+      <div className="container">
+        <p>button</p>
+        <p>button</p>
+      </div>
+    </div>
+  )
 }
 
 export default App
+
+const data = [
+  {
+    event: 'Admissão',
+    label: 'Analista de Software',
+    year: 2022,
+    icon: office,
+  },
+  {
+    event: 'Casamento',
+    label: 'Contraiu o Matrimonio',
+    year: 2023,
+    icon: rings,
+  },
+  {
+    event: 'Promoção',
+    label: '90mil',
+    year: 2024,
+    icon: office,
+  },
+  {
+    event: 'Casamento',
+    label: 'Contraiu o Matrimonio',
+    year: 2025,
+    icon: rings,
+  },
+  {
+    event: 'Promoção',
+    label: '100mil',
+    year: 2026,
+    icon: office,
+  },
+]
---
+++ history_view/src/components/timeline/timeline.tsx
@@ -0,0 +1,49 @@
+import './timeline.styles.scss'
+type timelineProps = {
+  history: { event: string; label: string; year: number; icon: string }[]
+}
+export const Timeline = ({ history }: timelineProps) => {
+  return (
+    <div className="timeline">
+      <div className="container">
+        <ol>
+          {history.map((item, index) => {
+            if (index % 2 === 0) {
+              return (
+                <li key={index}>
+                  <div className="img-handler">
+                    <img src={item.icon} alt="" />
+                  </div>
+                  <div className="ps-bot">
+                    <p>
+                      <b>{item.event}</b>
+                      <br />
+                      {item.label}
+                    </p>
+                  </div>
+                  <span className="span-top"></span>
+                </li>
+              )
+            } else {
+              return (
+                <li key={index}>
+                  <div className="img-handler">
+                    <img src={item.icon} alt="" />
+                  </div>
+                  <div className="ps-bot">
+                    <p className="p-bot">
+                      <b>{item.event}</b>
+                      <br />
+                      {item.label}
+                    </p>
+                  </div>
+                  <span className="span-bot"></span>
+                </li>
+              )
+            }
+          })}
+        </ol>
+      </div>
+    </div>
+  )
+}

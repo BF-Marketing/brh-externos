96359c42a0942d149d0236f74cae71e4df19cb14
Felipe Alves Louzeiro <felipe.louzeiro@external.bfmarketing.net>
2022-10-12 21:30:43 -0300

    ✨ history_view - feat: ajustando timeline

--- history_view/src/App.tsx
+++ history_view/src/App.tsx
@@ -1,7 +1,8 @@
-import { Timeline } from './components/timeline/timeline'
+import { Timeline } from './components/timeline'
 import rings from './assets/rings.svg'
 import office from './assets/office.svg'
 import { SalaryChart } from './components/charts/salary'
+import './App.styles.css'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Timeline history={data} />
       </div>
       <div className="container">
-        <SalaryChart />
+        <SalaryChart data={data} />
       </div>
     </div>
   )
@@ -22,30 +23,35 @@ const data = [
   {
     event: 'Admissão',
     label: 'Analista de Software',
+    value: 3000,
     year: 2022,
     icon: office,
   },
   {
     event: 'Casamento',
     label: 'Contraiu o Matrimonio',
+    value: 3000,
     year: 2023,
     icon: rings,
   },
   {
     event: 'Promoção',
-    label: '90mil',
+    value: 7000,
+    label: '7mil',
     year: 2024,
     icon: office,
   },
   {
     event: 'Casamento',
     label: 'Contraiu o Matrimonio',
+    value: 7000,
     year: 2025,
     icon: rings,
   },
   {
     event: 'Promoção',
-    label: '100mil',
+    label: '10mil',
+    value: 10000,
     year: 2026,
     icon: office,
   },

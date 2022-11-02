67bbe429c0355d43a54b14b4b959b5cda3cd0faa
Felipe Alves Louzeiro <felipe.louzeiro@external.bfmarketing.net>
2022-11-02 16:37:51 -0300

    feat: pagina support

--- brh-cliente--felipe/pages/support.js
+++ brh-cliente--felipe/pages/support.js
@@ -1,5 +1,47 @@
 import styles from '../styles/Pages.module.css'
 
 export default function Support() {
-  return <section className={styles['support-page']}>Suporte Técnico</section>
+  return (
+    <section className={styles['support-page']}>
+      <div className={styles['page-title-left']}>
+        <h1>Suporte Técnico</h1>
+        <p>Solucione o seu problema</p>
+        <form className={styles['support-form']}>
+          <div className={styles['form-group']}>
+            <label for="formSelect">Selecione o assunto do seu ticket</label>
+            <select id="formSelect" required>
+              <option value="" selected disabled hidden>
+                Selecione um assunto
+              </option>
+              <option value={'valor'}>2</option>
+              <option value={'valor'}>3</option>
+              <option value={'valor'}>4</option>
+              <option value={'valor'}>5</option>
+            </select>
+          </div>
+          <div className={styles['form-group']}>
+            <label for="formTextarea">Digite sua mensagem para o suporte</label>
+            <textarea required id="formTextarea"></textarea>
+          </div>
+          <div className={styles['form-buttons']}>
+            <button type="submit" className="btn">
+              Enviar
+            </button>
+            <button type="reset" className="btn">
+              Cancelar
+            </button>
+          </div>
+        </form>
+        <address className={styles['contact-sup']}>
+          <p>Para suporte técnico, contacte-nos:</p>
+          <span>+244 000 000 000</span>
+          <span>
+            <a href="mailto:Brhsoftware@comercial.com">
+              Brhsoftware@comercial.com
+            </a>
+          </span>
+        </address>
+      </div>
+    </section>
+  )
 }
